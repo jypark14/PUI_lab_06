d238ec892aea0fcb26e16c12e4549e0cdbe4523c
JY Park <jeyoungp@andrew.cmu.edu>
2018-10-15 10:57:31 -0400

    completed

--- main.js
+++ main.js
@@ -1,26 +1,54 @@
 function addNewList() {
-    alert('hello world!');
+  alert("hello world!");
 }
 
 function addListItem() {
-    var​ list ​=​ document​.​getElementById​(​"grocery-list"​);
-    var​ itemInput ​=​ document​.​getElementById​(​"new-list-item"​);    
-    var​ newItem ​=​ document​.​createElement​(​"li"​);
-    newItem​.​appendChild​(​document​.​createTextNode​(​itemInput​.value​));
-    list​.​appendChild​(​newItem​);
+  console.log("hello world");
+  var list = document.getElementById("grocery-list");
+  var itemInput = document.getElementById("new-list-item");
+  var newItem = document.createElement("li");
+  newItem.appendChild(document.createTextNode(itemInput.value));
+  list.appendChild(newItem);
 }
 
 function deleteListItem(item) {
-    // remove li element (item) from ol element (item.parentNode)
-    item.parentNode.removeChild(item);
+  // remove li element (item) from ol element (item.parentNode)
+  item.remove();
 }
 
 function completeListItem(item) {
-    if (item.checked) { // true if the input checkbox is checked
+  if (item.checked) {
+    // true if the input checkbox is checked
     item.parentNode.style.textDecoration = "line-through";
     // in css, this would be: "text-decoration: line-through"
-    } else {
+  } else {
     item.parentNode.style.textDecoration = "none";
     // in css, this would be: "text-decoration: none"
-    }
-    }
+  }
+}
+
+$(document).on("click", ".delete-item", function() {
+  $(this)
+    .parent()
+    .remove();
+});
+
+$(document).on("click", "#add-item", function() {
+  var list = $("#grocery-list");
+  var itemInput = $("#new-list-item");
+  list.append(
+    "<li>" + itemInput.val() + " <button class='delete-item'>X</button></li>"
+  );
+});
+
+$(document).on("click", ".complete-item", function() {
+  if ($(this).is(":checked")) {
+    $(this)
+      .parent()
+      .css("text-decoration", "line-through");
+  } else {
+    $(this)
+      .parent()
+      .css("text-decoration", "none");
+  }
+});
